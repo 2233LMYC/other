5e7672ade199e3686ff84d3fc0cb937bf6b71dbe
LMTX <15368311317@163.com>
2023-07-20 10:21:34 +0800

    测试用

--- Src/main.c
+++ Src/main.c
@@ -177,6 +177,8 @@ int main(void)
     OLED_ShowString(0,6,(uint8_t*)sbuf,12);
 
 
+      HAL_Delay(100);
+
     if(zhixing == 1)//Ö±ÏßÐÐÊ»
     {
         Motor(forward,default_dpeed,default_dpeed);
